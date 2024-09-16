10a4bbc29773629b3e9e45f34cc8646e82907b92
primrosemapombere <primrosemapombere@gmail.com>
2024-09-16 13:12:44 +0200

    added working JavaScript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings:
+      "Marinate your chicken pieces in spices of your choice for 4 hours",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let typeElement = document.querySelector("#type");
+typeElement.addEventListener("submit", generateRecipe);
